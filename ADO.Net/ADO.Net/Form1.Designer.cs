918f904839997edf127b77bf9e0cef9a50ce6f3f
PrashantSNimbalkar <107276876+PrashantSNimbalkar@users.noreply.github.com>
2022-07-18 19:19:36 +0530

    Add files via Upload

---
+++ ADO.Net/ADO.Net/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿
+namespace ADO.Net
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textId = new System.Windows.Forms.TextBox();
+            this.textName = new System.Windows.Forms.TextBox();
+            this.textSalary = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnSearchById = new System.Windows.Forms.Button();
+            this.EmpGridView = new System.Windows.Forms.DataGridView();
+            this.btnShowAllEmp = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.EmpGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(67, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Emp Id";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(64, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Emp Name";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(64, 156);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Emp Salary";
+            // 
+            // textId
+            // 
+            this.textId.Location = new System.Drawing.Point(186, 37);
+            this.textId.Name = "textId";
+            this.textId.Size = new System.Drawing.Size(100, 22);
+            this.textId.TabIndex = 3;
+            // 
+            // textName
+            // 
+            this.textName.Location = new System.Drawing.Point(186, 92);
+            this.textName.Name = "textName";
+            this.textName.Size = new System.Drawing.Size(100, 22);
+            this.textName.TabIndex = 4;
+            // 
+            // textSalary
+            // 
+            this.textSalary.Location = new System.Drawing.Point(186, 156);
+            this.textSalary.Name = "textSalary";
+            this.textSalary.Size = new System.Drawing.Size(100, 22);
+            this.textSalary.TabIndex = 5;
+            this.textSalary.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(70, 264);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 6;
+            this.btnSave.Text = "save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(211, 264);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 7;
+            this.btnUpdate.Text = "update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(341, 264);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 8;
+            this.btnDelete.Text = "delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnSearchById
+            // 
+            this.btnSearchById.Location = new System.Drawing.Point(341, 33);
+            this.btnSearchById.Name = "btnSearchById";
+            this.btnSearchById.Size = new System.Drawing.Size(131, 36);
+            this.btnSearchById.TabIndex = 9;
+            this.btnSearchById.Text = "SearchByID";
+            this.btnSearchById.UseVisualStyleBackColor = true;
+            this.btnSearchById.Click += new System.EventHandler(this.btnSearchById_Click);
+            // 
+            // EmpGridView
+            // 
+            this.EmpGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.EmpGridView.Location = new System.Drawing.Point(528, 33);
+            this.EmpGridView.Name = "EmpGridView";
+            this.EmpGridView.RowHeadersWidth = 51;
+            this.EmpGridView.RowTemplate.Height = 24;
+            this.EmpGridView.Size = new System.Drawing.Size(405, 211);
+            this.EmpGridView.TabIndex = 10;
+            // 
+            // btnShowAllEmp
+            // 
+            this.btnShowAllEmp.Location = new System.Drawing.Point(712, 264);
+            this.btnShowAllEmp.Name = "btnShowAllEmp";
+            this.btnShowAllEmp.Size = new System.Drawing.Size(75, 23);
+            this.btnShowAllEmp.TabIndex = 11;
+            this.btnShowAllEmp.Text = "Show All Emp";
+            this.btnShowAllEmp.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1074, 450);
+            this.Controls.Add(this.btnShowAllEmp);
+            this.Controls.Add(this.EmpGridView);
+            this.Controls.Add(this.btnSearchById);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.textSalary);
+            this.Controls.Add(this.textName);
+            this.Controls.Add(this.textId);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.EmpGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textId;
+        private System.Windows.Forms.TextBox textName;
+        private System.Windows.Forms.TextBox textSalary;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnSearchById;
+        private System.Windows.Forms.DataGridView EmpGridView;
+        private System.Windows.Forms.Button btnShowAllEmp;
+    }
+}
+
---
+++ ADO.Net/ADO.Net/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+
+
+namespace ADO.Net
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection con;
+        SqlCommand cmd;
+        SqlDataReader dr;
+
+        public Form1()
+        {
+            InitializeComponent();
+            //step1
+            con = new SqlConnection(ConfigurationManager.ConnectionStrings["dbConnection"].ConnectionString);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //step2 write a query that needs to be fire
+                string qry = "insert into employeedetails values(@emp_name,@emp_salary)";
+                //step 3 assign qry to the command
+                cmd = new SqlCommand(qry, con);
+                // step 4  assign parameter value
+                cmd.Parameters.AddWithValue("@name", textName.Text);
+                cmd.Parameters.AddWithValue("@salary", Convert.ToDouble(textSalary.Text));
+                // step 5 open the connection to fire query
+                con.Open();
+                // step 6 fire the query
+                int result = cmd.ExecuteNonQuery();
+                if (result == 1)
+                {
+                    MessageBox.Show("Success ! Record inserted");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSearchById_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string qry = "select * from employeedetails where emp_id=@id";
+                cmd = new SqlCommand(qry, con);
+                cmd.Parameters.AddWithValue("@id", Convert.ToInt32(textId.Text));
+                con.Open();
+                dr = cmd.ExecuteReader();
+                if (dr.HasRows)
+                {
+                    while (dr.Read())
+                    {
+                        textName.Text = dr["emp_name"].ToString();// Name , Salary are the col names
+                        textSalary.Text = dr["emp_salary"].ToString();
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Record not found");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                //step2 write a query that needs to be fire
+                string qry = "update employeedetails set emp_name=@name, emp_salary=@salary where emp_id=@id;";
+                //step 3 assign qry to the command
+                cmd = new SqlCommand(qry, con);
+                // step 4  assign parameter value
+                cmd.Parameters.AddWithValue("@name", textName.Text);
+                cmd.Parameters.AddWithValue("@salary", Convert.ToDouble(textSalary.Text));
+                cmd.Parameters.AddWithValue("@id", Convert.ToInt32(textId.Text));
+                // step 5 open the connection to fire query
+                con.Open();
+                // step 6 fire the query
+                int result = cmd.ExecuteNonQuery();
+                if (result == 1)
+                {
+                    MessageBox.Show("Success ! Record updated");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+
+        }
+
+       
+
+    }
+}
